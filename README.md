a0567716323b4d7bb424e63e44ad953018b3f6d1
idktupalo <kostya.tupalo@gmail.com>
2023-02-14 22:03:21 +0200

    add django-todo-app v1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # django_todo_with_auth
 Django todo list with user authentication
+
+    Main page
+![main page](./result/main_page.jpg)
+
+    Search query
+![search_query](./result/search_query.jpg)
+
+    Add/edit task
+![add-edit](./result/add-edit.jpg)
+
+    Delete task confiramtion
+![delete-task](./result/del-task.jpg)
+
+    Login/Register page
+![login](./result/login.jpg)
+![register](./result/register.jpg)
---
+++ todo_app/base/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'task'
+        verbose_name_plural = 'tasks'
+        ordering = ['complete']
